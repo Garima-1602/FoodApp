b58a6e67433ab39cc024f51a4845753d1aeff42a
Garima <garima89077@gmail.com>
2022-07-11 11:21:17 +0530

    Bundle creation

--- app/src/main/java/com/example/foodapp/LoginActivity.kt
+++ app/src/main/java/com/example/foodapp/LoginActivity.kt
@@ -35,5 +35,21 @@ class LoginActivity : AppCompatActivity() {
             startActivity(Intent(this@LoginActivity,RegisterActivity::class.java))
         }
         title="Log In"
+        btnLogin.setOnClickListener()
+        {
+             val bundle=Bundle() //to send data from one activity ro another
+            //key-value pair
+            //here key is mobile no and value is data which is retrieved from user
+            //putString to  store value in bundle taken from user
+            bundle.putString("Mobile No",etPhone.text.toString())
+            bundle.putString("Password",etPassword.text.toString())
+
+            //putExtra is to send the retrieve data from one activity to another
+            intent.putExtra("information",bundle)
+            //intent creation to connect it to another activity
+            startActivity(Intent(this@LoginActivity,DashboardActivity::class.java))
+
+
+        }
     }
 }
