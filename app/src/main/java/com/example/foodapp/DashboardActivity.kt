42473ca9ab2d40f92d12ccfad1273427e2eace10
Garima <garima89077@gmail.com>
2022-07-11 11:38:20 +0530

    bundle creation

--- app/src/main/java/com/example/foodapp/DashboardActivity.kt
+++ app/src/main/java/com/example/foodapp/DashboardActivity.kt
@@ -2,10 +2,22 @@ package com.example.foodapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class DashboardActivity : AppCompatActivity() {
+    lateinit var txtReceived:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+        txtReceived=findViewById(R.id.txtReceived)
+        if(intent!=null)
+        {
+            val details = intent.getBundleExtra("details")
+            txtReceived.text= details.toString()
+        }
+        else
+        {
+            txtReceived.text="No data found!"
+        }
     }
 }
