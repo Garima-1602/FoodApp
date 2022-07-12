f13eb79017cb0107f64f4c05a14f4fc8c10a65bb
Garima <garima89077@gmail.com>
2022-07-12 20:07:11 +0530

    bundle creation

--- app/src/main/java/com/example/foodapp/LoginActivity.kt
+++ app/src/main/java/com/example/foodapp/LoginActivity.kt
@@ -10,7 +10,7 @@ import org.w3c.dom.Text
 
 class LoginActivity : AppCompatActivity() {
     //declaring the views
-    lateinit var etPhone:EditText
+    lateinit var etEmail:EditText
     lateinit var etPassword:EditText
     lateinit var btnLogin:Button
     lateinit var txtForgot:TextView
@@ -19,7 +19,7 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         //initialising the views
-        etPhone=findViewById(R.id.etPhone)
+        etEmail=findViewById(R.id.etEmail)
         etPassword=findViewById(R.id.etPassword)
         txtForgot=findViewById(R.id.txtForgot)
         txtRegister=findViewById(R.id.txtRegister)
@@ -37,18 +37,19 @@ class LoginActivity : AppCompatActivity() {
         title="Log In"
         btnLogin.setOnClickListener()
         {
+            val intent=Intent(this@LoginActivity,DashboardActivity::class.java)
              val bundle=Bundle() //to send data from one activity ro another
             //key-value pair
             //here key is mobile no and value is data which is retrieved from user
             //putString to  store value in bundle taken from user
-
-            bundle.putString("Mobile No",etPhone.text.toString())
+            bundle.putString("data", "login")
+            bundle.putString("Enail",etEmail.text.toString())
             bundle.putString("Password",etPassword.text.toString())
 
             //putExtra is to send the retrieve data from one activity to another
             intent.putExtra("details",bundle)
             //intent creation to connect it to another activity
-            startActivity(Intent(this@LoginActivity,DashboardActivity::class.java))
+            startActivity(intent)
 
 
         }
