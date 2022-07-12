e9599094ab7dc025ee8de393215a3bec2fcab793
Garima <garima89077@gmail.com>
2022-07-12 23:05:31 +0530

    updated

--- app/src/main/java/com/example/foodapp/DashboardActivity.kt
+++ app/src/main/java/com/example/foodapp/DashboardActivity.kt
@@ -10,6 +10,43 @@ class DashboardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
         txtReceived=findViewById(R.id.txtReceived)
+ //as intent is used to connect one activity to another
+        //now to check if any data is received or not
+        if(intent!=null)
+        {
+            //to fetch details from intent we use getExtra
+            val details=intent.getBundleExtra("details") //take
+            //getting the value of data
+            val data= details?.getString("data")
+            if(data=="login")
+            {
+                val text="Email: ${details.getString("email")} \n" +
+                        "Password : ${details.getString("password")}"
+                txtReceived.text=text
+            }
+            if(data=="forgot")
+            {
+                val text="Email: ${details.getString("email")} \n" +
+                        "Password : ${details.getString("password")}"
+                txtReceived.text=text
+            }
+            if(data=="register")
+            {
+                val text="Name ${details.getString("name")}\n"+
+                        "Email: ${details.getString("email")} \n" +
+                        "Mobile No:  ${details.getString("mobileno")}\n" +
+                        "Address : ${details.getString("address")}\n" +
+                        "Password : ${details.getString("password")}" +
+                        "Confirm Password: ${details.getString("confirm password")}"
+                txtReceived.text=text
+            }
+
+
+        }
+        else
+        {
+            txtReceived.text="No data Recevied"
+        }
 
 
     }
